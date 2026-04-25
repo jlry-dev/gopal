b9966f33b593f00e1abf2fe5e188bcf0a02287fa
jlry-dev <personal.jlry@gmail.com>
2026-04-26 02:00:10 +0800

    feat: added may start next check on track end event

--- handlers/event_handlers.go
+++ handlers/event_handlers.go
@@ -28,6 +28,10 @@ func OnTrackStart(r ReplyHandler) func(disgolink.Player, lavalink.TrackStartEven
 
 func OnTrackEnd(queueManager queue.QueueManager) func(disgolink.Player, lavalink.TrackEndEvent) {
 	return func(player disgolink.Player, e lavalink.TrackEndEvent) {
+		if !e.Reason.MayStartNext() {
+			return
+		}
+
 		queue := queueManager.Get(e.GuildID())
 
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
